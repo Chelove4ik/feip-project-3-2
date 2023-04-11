d5fa6d5a77351972b2cc8b21dd8b2ba0197960c0
Denis <dennis220802@gmail.com>
2023-04-11 19:45:52 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import render_template
+
+from backend.config import app
+
+
+@app.route('/')
+def index():
+    name = "Test"
+    return render_template('index.html', name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
